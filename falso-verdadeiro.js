d55567c41f2619199c1a26e2be07ec5aa5f01038
joselabiak-creator <jose.labiak@escola.pr.gov.br>
2026-04-30 15:21:47 +0000

    salva

--- falso-verdadeiro.js
+++ falso-verdadeiro.js
@@ -1,3 +1,4 @@
+
 //booleano
 
 const usuarioLogado = true;
---
+++ script.js
@@ -0,0 +1,15 @@
+// exemplo de tipagem de dinamica
+//let dado = 100;
+//console.log("valor", dado);
+//console.log("tipo", typeof dado);
+
+// mudando o tipo da mesma variavel(o que define a tipagem dinamica)
+//dado = "ola , sou um texto";
+//console.log("novo valor" , dado);
+//console.log("novo tipo" , typeof dado);
+
+//let dado = 412;
+//console.log("valor", dado);
+//console.log("tipo", typeof dado);
+const cpf = "100.938.469-40";
+console.log("cpf" , cpf)
