9314e0655ff6c66731f5c73e16f849b3a52807d2
joselabiak-creator <jose.labiak@escola.pr.gov.br>
2026-05-21 15:07:21 +0000

    funcao

---
+++ funcoes.js
@@ -0,0 +1,9 @@
+const numero1 = 11;
+const numero2 = 2;
+
+/* multiplicação */
+function multiplicacao(num1, num2) {
+      return num1 * num2
+}
+console.log(multiplicacao(numero1, numero2));
+console.log(multiplicacao (4 ,5));
---
+++ templete-string.js
@@ -0,0 +1,6 @@
+const nome = "jose eduardo";
+const idade = 2026-2010;
+const cidadeNacimento = "turvo";
+const apresentacao = "meu nome é " + nome + ", minha idade é " + idade +"  e nasci na cidade " + cidadeNacimento;
+
+console.log(apresentacao);
